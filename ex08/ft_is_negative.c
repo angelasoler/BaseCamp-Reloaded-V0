f40aa107a935350b4291636b9cdbb61ea1dc288e
asoler <asoler@student.42sp.org.br>
2022-03-23 19:27:21 +0100

    displaying N ou P depending on the integers sign

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/23 19:16:20 by asoler            #+#    #+#             */
+/*   Updated: 2022/03/23 19:20:42 by asoler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
+
+int	main(void)
+{
+	ft_is_negative(-45);
+}
+
