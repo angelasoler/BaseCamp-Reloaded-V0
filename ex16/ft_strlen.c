2db7023bb5a1ee6a5e5ee35d5d6640ef4a355267
asoler <asoler@student.42sp.org.br>
2022-03-24 15:47:03 +0100

    this funtion return the length of a string passing as a parameter

---
+++ ex16/ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 15:34:13 by asoler            #+#    #+#             */
+/*   Updated: 2022/03/24 15:46:04 by asoler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+#include <stdio.h>
+#include <string.h>
+int	main()
+{
+	char *str = "these string has 25 chars";
+
+	printf("Original Funtion: %ld\n", strlen(str));
+	printf("Ft Funtion: %d\n", ft_strlen(str));
+}
+
