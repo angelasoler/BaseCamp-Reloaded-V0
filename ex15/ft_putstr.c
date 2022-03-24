cb2b7622a294ee80976a4a9e639cb9733494925e
asoler <asoler@student.42sp.org.br>
2022-03-24 15:30:06 +0100

    these funtion display a string on the stdout

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 15:25:20 by asoler            #+#    #+#             */
+/*   Updated: 2022/03/24 15:29:26 by asoler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr("Hey, display this string of characters please");
+}
